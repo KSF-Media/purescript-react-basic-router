cef97eb9c4126e61136e70f771a184368847b4c5
ae-mo <morciano.andrea92@gmail.com>
2020-12-10 16:36:49 +0200

    Refactor Router.Foreign module
    * Factor datatypes out in a separate module

---
+++ src/Router/Types.purs
@@ -0,0 +1,47 @@
+module React.Basic.Router.Types where
+
+import Data.Nullable (Nullable)
+import Foreign (Foreign)
+import React.Basic (JSX, ReactComponent)
+
+type JSRouterProps a = { location :: Location a }
+
+type Match =
+  { params :: Foreign
+  , isExact :: Boolean
+  , path :: String
+  , url :: String
+  }
+
+type Location state =
+  { key :: String
+  , pathname :: String
+  , search :: String
+  , hash :: String
+  , state :: Nullable state
+  }
+
+type BaseRedirectProps state =
+  ( to   :: To state
+  , from :: String
+  , push :: Boolean
+  )
+
+type RedirectProps state = Record (BaseRedirectProps state)
+
+type DelayedRedirectProps state = { delay :: Number | BaseRedirectProps state }
+
+type To state_ =
+  { pathname :: String
+  , state :: state_
+  }
+
+type SwitchProps = { children :: Array JSX }
+
+type RouteProps jsProps =
+  { exact :: Boolean
+  , path :: Nullable String
+  , render :: jsProps -> JSX
+  }
+
+type LinkProps props state = { to :: To state, children :: Array JSX, className :: String }
