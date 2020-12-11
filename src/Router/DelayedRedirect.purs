b898584e0ce14324a5b4f297e3b42541986d8fbb
ae-mo <morciano.andrea92@gmail.com>
2020-12-11 15:33:24 +0200

    Fix Router.DelayedRedirect component
    * Use didMount in component creation

--- src/Router/DelayedRedirect.purs
+++ src/Router/DelayedRedirect.purs
@@ -4,9 +4,11 @@ import Prelude
 
 import Effect (Effect)
 import Effect.Class (liftEffect)
+import Effect.Class.Console as Console
 import Effect.Aff as Aff
 import React.Basic (JSX, element, make)
 import React.Basic as React
+import React.Basic.DOM as DOM
 import React.Basic.Router.Foreign as Router
 import React.Basic.Router.Types
 
@@ -15,30 +17,26 @@ type State = { redirectState :: RedirectState }
 data RedirectState = Wait | Redirect
 
 delayedRedirect :: forall state. (DelayedRedirectProps state) -> JSX
-delayedRedirect = make component { initialState, render }
+delayedRedirect = make component { initialState, render, didMount }
+  where
+    initialState = { redirectState: Wait }
+
+    didMount self@{ props: { delay }, setState, state } = do
+      Aff.launchAff_ do
+        Aff.delay $ Aff.Milliseconds delay
+        liftEffect $ do setState _ { redirectState = Redirect }
+
+    render self@{ props: { from, push, to }, state: { redirectState } } =
+      case redirectState of
+        Wait     -> mempty
+        Redirect ->
+          let redirectProps =
+                { from: from
+                , push: push
+                , to: to
+                }
+          in
+            element Router.redirect redirectProps
 
 component :: forall state. React.Component (DelayedRedirectProps state)
 component = React.createComponent "DelayedRedirect"
-
-initialState :: State
-initialState =
-  { redirectState: Wait
-  }
-
-didMount :: forall state. React.Self (DelayedRedirectProps state) State -> Effect Unit
-didMount self@{ props: { delay }, setState } = Aff.launchAff_ do
-  Aff.delay $ Aff.Milliseconds delay
-  liftEffect $ setState _ { redirectState = Redirect }
-
-render :: forall state. React.Self (DelayedRedirectProps state) State -> JSX
-render self@{ props: { from, push, to }, state: { redirectState } } = 
-  case redirectState of
-    Wait     -> mempty
-    Redirect ->
-      let redirectProps = 
-            { from: from
-            , push: push
-            , to: to 
-            }
-      in
-        element Router.redirect redirectProps
